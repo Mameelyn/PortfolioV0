c8c04849e2933fa55f14d346af424c68f6b9606d
022002._ <135936272+Mameelyn@users.noreply.github.com>
2023-07-22 10:26:06 +0700

    set up project

---
+++ PorfolioV0-Project/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>  
+    <Portfolio/>
+  </div>
+</template>
+
+<script setup>
+import Portfolio from './components/Portfolio.vue';
+</script>
+
+<style scoped>
+  
+</style>
---
+++ PorfolioV0-Project/src/components/Portfolio.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <Introduction/>
+    </div>
+</template>
+
+<script setup>
+import Introduction from './Section1/Introduction.vue'
+</script>
+
+<style scoped>    
+</style>
